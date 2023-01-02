d52efdb57c72715a4888665c794e09ca632cc25b
S3BASCH <sebaschuquiguanga@gmail.com>
2023-01-02 16:15:18 -0500

    Pagina 1 listaaaaaa

---
+++ compsyusoo/src/app/componentes/pagina1/pagina1.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styleUrls: ['./pagina1.component.css']
+})
+export class Pagina1Component implements OnInit {
+  resultado:any;
+  suma:number
+  dato:number
+  array=[23,12,45,41,89]
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+  almacenar(){
+    localStorage.setItem('dato',this.dato.toString())
+  }
+  
+  recuperar(){
+    this.resultado=localStorage.getItem('dato')
+    this.suma=2+Number(this.resultado)
+  }
+  
+  
+    navegar(){
+      this.router.navigate(['pag2'])
+    }
+}
