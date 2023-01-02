5135ba8b49304fd2a23b6536b618ed7a44691b1c
S3BASCH <sebaschuquiguanga@gmail.com>
2023-01-02 17:35:46 -0500

    ya pudeeeeeeee

--- compsyusoo/src/app/app-routing.module.ts
+++ compsyusoo/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Pagina2Component } from './componentes/pagina2/pagina2.component';
 
 const routes: Routes = [
   {
-    path:'Pagina 1', component:Pagina1Component
+    path:'', component:Pagina1Component
   },
   {
     path:'Pagina 2',component:Pagina2Component
