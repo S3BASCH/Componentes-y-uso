6e7650132d00f7046cbfde623b168df2422dabfe
S3BASCH <sebaschuquiguanga@gmail.com>
2023-01-02 16:10:38 -0500

    Creacion de rutas

--- compsyusoo/src/app/app.module.ts
+++ compsyusoo/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Pagina1Component } from './componentes/pagina1/pagina1.component';
+import { Pagina2Component } from './componentes/pagina2/pagina2.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component,
+    Pagina2Component
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
